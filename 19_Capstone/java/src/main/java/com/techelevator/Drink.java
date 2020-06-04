69d6b08bb7ddebf8506b1f6738f59df400c66a73
Doyle ODonnell <doyleodo@yahoo.com>
2020-06-04 12:09:18 -0400

    implemented vendable classes

---
+++ 19_Capstone/java/src/main/java/com/techelevator/Drink.java
@@ -0,0 +1,58 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+public class Drink implements Vendable {
+
+	private String name;
+	private BigDecimal price;
+	private String sound;
+	private String slot;
+	private int stock;
+	private boolean soldOut;
+	
+	public Drink(String name, BigDecimal price, String slot) {
+		this.name = name;
+		this.price = price;
+		this.slot = slot;
+		this.sound = "Glug Glug, yum!";
+		this.stock = 5;
+		this.soldOut = false;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	@Override
+	public String getSound() {
+		return sound;
+	}
+
+	@Override
+	public String getSlot() {
+		return slot;
+	}
+
+	@Override
+	public String getStock() {
+		if (!soldOut) {
+		return Integer.toString(stock);
+		} else return "SOLD OUT";
+	}
+
+	@Override
+	public void buy() {
+		this.stock--;
+		if (stock == 0) {
+			soldOut = true;
+		}
+	}
+
+}
---
+++ 19_Capstone/java/src/main/java/com/techelevator/Vendable.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+public interface Vendable {
+
+	public String getName();
+	public BigDecimal getPrice();
+	public String getSound();
+	public String getSlot();
+	public String getStock();
+	public void buy();
+}
